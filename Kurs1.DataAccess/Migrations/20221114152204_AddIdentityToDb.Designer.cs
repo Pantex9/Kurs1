d471add95d46cdba2ae45a970dcc13cffd1fb13a
Milos Pantelic <milos.pantelic340@gmail.com>
2022-11-14 16:41:44 +0100

    final 14.11

--- Kurs1.DataAccess/Migrations/20221114152204_AddIdentityToDb.Designer.cs
+++ Kurs1.DataAccess/Migrations/20221114152204_AddIdentityToDb.Designer.cs
@@ -4,6 +4,7 @@ using Kurs1.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kurs1.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221114152204_AddIdentityToDb")]
+    partial class AddIdentityToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
